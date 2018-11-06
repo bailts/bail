734d9fde3e0b0d7b6a6acc51b9b2ed88218d2b0e
RandyWardhana <randywardhana37@gmail.com>
2018-11-06 16:57:01 +0700

    Finish User Model

--- src/model/employee/User.ts
+++ src/model/employee/User.ts
@@ -1,4 +1,5 @@
 import { employeeConnection as db } from '../../module/database'
+import { Result } from 'range-parser';
 
 const _result = { success: false, message: "", error: null }
 
@@ -31,12 +32,37 @@ class User {
         }
     }
 
-    // public static async update(username: String, password: String, fullname: String, level: Number) {
-    //     try {
-    //         const success = await db('user').insert({ username, password, fullname, level })
-    //         if (success.length > 0) { }
-    //     }
-    // }
+    public static async update(id: Number, username: String, password: String, fullname: String, level: Number) {
+        const result = _result
+        try {
+            const success = await db('user').update({ username, password, fullname, level }).where({ id })
+            if (success == 1) {
+                result.success = true
+                result.message = "Success"
+            } else {
+                result.message = "Unable to update User "
+            }
+            return result
+        } catch (err) {
+            throw err
+        }
+    }
+
+    public static async delete(id: Number) {
+        const result = _result
+        try {
+            const success = await db('user').delete().where({ id })
+            if (success == 1) {
+                result.success = true
+                result.message = "Success"
+            } else {
+                result.message = "Unable to Delete User"
+            }
+            return result
+        } catch(err) {
+            throw err
+        }
+    }
 
 }
 
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -7,13 +7,22 @@ Router.get('/', async (req, res) => {
     res.send(await User.get())
 })
 
+Router.post('/new', async (req, res) => {
+    const { username, password, fullname, level } = req.body
+    res.send(await User.insert(username, password, fullname, level))
+})
+
 Router.get('/:id', async (req, res) => {
     res.send(await User.get(req.params.id))
 })
 
-Router.post('/new', async (req, res) => {
-    const {username, password,fullname,level} = req.body
-    res.send(await User.insert(username, password, fullname, level))
+Router.put('/:id/update', async (req, res) => {
+    const { id, username, password, fullname, level } = req.body
+    res.send(await User.update(id, username, password, fullname, level))
+})
+
+Router.get('/:id/delete', async (req, res) => {
+    res.send(await User.delete(req.params.id))
 })
 
 export default Router
