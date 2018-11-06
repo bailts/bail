aea12056ec858c9ce75676fb1b1c6acfe9d066f3
RandyWardhana <randywardhana37@gmail.com>
2018-11-06 12:56:09 +0700

    Privileges Database

--- src/database/privileges.sql
+++ src/database/privileges.sql
@@ -2,6 +2,10 @@ CREATE USER 'admin'@'localhost' IDENTIFIED BY 'adm00n';
 CREATE USER 'employee'@'localhost' IDENTIFIED BY 'empl00yee';
 
 GRANT * ON bail.* TO 'admin@localhost';
-GRANT * ON bail.* TO 'employee@localhost';
+
+GRANT * ON bail.user TO 'employee@localhost';
+GRANT * ON bail.mail TO 'employee@localhost';
+GRANT * ON bail.disposition TO 'employee@localhost';
+GRANT SELECT ON bail.mailtype TO 'employee@localhost';
 
 FLUSH PRIVILEGES;
