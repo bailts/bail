8f3522b48a127cdf9d13542dd27cac101327eb04
Adrian M. R <adrianmridwan@gmail.com>
2018-11-08 11:09:51 +0700

    Add admin user model

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,32 +1,33 @@
 import * as express from 'express'
-import User from '../model/employee/User'
+import EmployeeUser from '../model/employee/User'
+import AdminUser from '../model/admin/User'
 
 const Router = express.Router()
 
 Router.get('/', async (req, res) => {
-    res.send(await User.get())
+    res.send(await EmployeeUser.get())
 })
 
 Router.post('/new', async (req, res) => {
-    const {username, password,fullname,level} = req.body
-    let data = await User.insert(username, password, fullname, level)
-    if(req.accepts('html'))
+    const { username, password, fullname, level } = req.body
+    let data = await EmployeeUser.insert(username, password, fullname, level)
+    if (req.accepts('html'))
         res.redirect('/users')
     else
         res.send(data)
 })
 
 Router.get('/:id', async (req, res) => {
-    res.send(await User.get(req.params.id))
+    res.send(await EmployeeUser.get(req.params.id))
 })
 
 Router.put('/:id/update', async (req, res) => {
     const { username, password, fullname, level } = req.body
-    res.send(await User.update(req.params.id, username, password, fullname, level))
+    res.send(await EmployeeUser.update(req.params.id, username, password, fullname, level))
 })
 
 Router.get('/:id/delete', async (req, res) => {
-    res.send(await User.delete(req.params.id))
+    res.send(await AdminUser.delete(req.params.id))
 })
 
 export default Router
